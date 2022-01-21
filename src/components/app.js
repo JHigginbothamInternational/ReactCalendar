23afa7ea1607c37a3e0e4b5425e2424860288b3d
jacobhigginbotham <j.higginbotham.international@gmail.com>
2022-01-20 20:31:11 -0600

    Dummy data feature added for the months

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import Header from "./header"
 import Footer from "./footer"
 
+
+import dummyData from "../../static/assets/dummyData"
+
 export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monthData: dummyData.data[0]
+    }
+  }
+
   render() {
     return (
       <div className='page-wrapper'>
-        <Header month="January" />
-        <Footer year="2022" />
+        <Header month={this.state.monthData.name} />
+        <Footer year={this.state.monthData.year} />
       </div>
     );
   }
